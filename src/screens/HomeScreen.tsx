57afa4a0892d55b85d40ded696e26814876d9e8d
oscar120020 <oscarmiguel12002@gmail.com>
2022-12-28 17:55:40 -0400

    Basic config

---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { View } from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+export const HomeScreen = () => {
+  return (
+    <View>
+        <Icon name="copy-outline" color="red" size={80} />
+    </View>
+  );
+};
